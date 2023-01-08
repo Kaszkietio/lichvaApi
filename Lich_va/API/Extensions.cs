97f841d52775191e84c234e09d662fa311fe9be3
Kaszkietio <waxer459@gmail.com>
2023-01-08 21:59:44 +0100

    xd no ext

---
+++ Lich_va/API/Extensions.cs
@@ -0,0 +1,38 @@
+﻿using API.Dtos;
+using BankDataLibrary.Entities;
+
+namespace API
+{
+    public static class Extensions
+    {
+        public static InquireDto AsDto(this Inquire inquire)
+        {
+            InquireDto result = new()
+            {
+                Id = inquire.Id,
+                CreationDate = inquire.CreationDate,
+                UserId = inquire.UserId,
+                Ammount = inquire.Ammount,
+                Installments = inquire.Installments,
+            };
+            return result;
+        }
+        public static OfferDto AsDto(this Offer offer)
+        {
+            OfferDto result = new()
+            {
+                Id = offer.Id,
+                CreationDate = offer.CreationDate,
+                UserId = offer.UserId,
+                Ammount = offer.Ammount,
+                Installments = offer.Installments,
+                BankId = offer.BankId,
+                GeneratedContract = offer.GeneratedContract,
+                OfferStatus = offer.OfferStatus.ToString(),
+                PlatformId = offer.PlatformId,
+                SignedContract = offer.SignedContract,
+            };
+            return result;
+        }
+    }
+}
