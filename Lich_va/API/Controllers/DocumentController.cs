fb353846161bbefd1e94368f7e0e8068baf3fa7c
Kaszkietio <waxer459@gmail.com>
2023-01-17 23:34:28 +0100

    well its gonna be funny

---
+++ Lich_va/API/Controllers/DocumentController.cs
@@ -0,0 +1,28 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentController : ControllerBase
+    {
+        private static readonly string _connectionString = "DefaultEndpointsProtocol=https;AccountName=lichvablob;AccountKey=GViAL6dnsvV4lqPjCtyKmZS+cftwG9101FzxPoeTdCu43wczTumxr9CE5IZZSIsGf5uomIEmJID7+AStik1orQ==;EndpointSuffix=core.windows.net";
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> UploadFile(IList<IFormFile> files)
+        {
+            BlobContainerClient client = new BlobContainerClient(_connectionString, "lichvapdf");
+            foreach(var file in files)
+            {
+                using MemoryStream stream = new();
+                await file.CopyToAsync(stream);
+                stream.Position = 0;
+                await client.UploadBlobAsync(file.FileName, stream);
+            }
+            return Ok("Successfully uploaded files");
+        }
+
+    }
+}
