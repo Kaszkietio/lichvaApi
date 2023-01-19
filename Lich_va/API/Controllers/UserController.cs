c00b174e00bb248945e7eea01c8f41b095dc8223
Kaszkietio <waxer459@gmail.com>
2023-01-19 10:29:55 +0100

    oiuh

--- Lich_va/API/Controllers/UserController.cs
+++ Lich_va/API/Controllers/UserController.cs
@@ -16,17 +16,45 @@ namespace API.Controllers
         IBankRepository Repository { get; init; }
         public UserController(IBankRepository repo) { Repository = repo; }
 
-        //[HttpGet]
-        //public async Task<ActionResult<IEnumerable<GetUserDto>>> GetAllAsync()
-        //{
-        //    return Ok((await Repository.GetUsersAsync()).Select(x => x.AsDto()));
-        //}
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<GetUserDto>>> GetAllAsync(
+            [FromHeader] string authToken,
+            [FromQuery] IList<int> idFilter,
+            [FromQuery] IList<DateTime> createDateFilter,
+            [FromQuery] IList<string> emailFilter,
+            [FromQuery] IList<int> roleFilter,
+            [FromQuery] IList<bool> internalFilter,
+            [FromQuery] IList<bool> anonymousFilter,
+            [FromQuery] IList<string> hashFilter
+            )
+        {
+            try
+            {
+                await Repository.AuthenticateUserAsync(authToken);
+                return Ok((await Repository.GetUsersAsync()).Select(x => x.AsGetDto()));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { ex.Message });
+            }
+        }
 
-        //[HttpPut]
-        //public async Task<ActionResult> Update([FromBody]UpdateUserDto user)
-        //{
-        //    await Repository.UpdateUserAsync(user);
-        //    return Ok();
-        //}
+        [HttpPut]
+        public async Task<ActionResult> Update(
+            [FromHeader] string authToken,
+            [FromBody] UpdateUserDto user
+            )
+        {
+            try
+            {
+                User authUser = await Repository.AuthenticateUserAsync(authToken);
+                await Repository.UpdateUserAsync(authUser.Id, user);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { ex.Message });
+            }
+        }
     }
 }
