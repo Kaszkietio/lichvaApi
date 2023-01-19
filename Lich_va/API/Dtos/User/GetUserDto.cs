8df36abe0e9d84341655a6d7a55055d11c3d5b7c
Kaszkietio <waxer459@gmail.com>
2023-01-19 09:32:50 +0100

    xd

---
+++ Lich_va/API/Dtos/User/GetUserDto.cs
@@ -0,0 +1,27 @@
+﻿namespace API.Dtos.User
+{
+    public class GetUserDto
+    {
+        public DateTime? CreationDate { get; set; }
+
+        public int? RoleId { get; set; }
+
+        public bool? Active { get; set; }
+
+        public bool? Anonymous { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public int? JobTypeId { get; set; }
+
+        public int? IncomeLevel { get; set; }
+
+        public int? IdTypeId { get; set; }
+
+        public string? IdNumber { get; set; }
+    }
+}
